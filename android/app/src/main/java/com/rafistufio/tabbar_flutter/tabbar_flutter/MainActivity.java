c363c73b4c3a79f1cfff071f71b74566c7cd6a1a
Tanvir Anwar <tanvir.anwar2@gmail.com>
2020-06-30 11:35:32 +0600

    tabbar

---
+++ android/app/src/main/java/com/rafistufio/tabbar_flutter/tabbar_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rafistufio.tabbar_flutter.tabbar_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/category.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoryPage(),
+    );
+  }
+}
+
+class CategoryPage extends StatefulWidget {
+
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(child: Icon(Icons.category, size: 150.0, color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+}
